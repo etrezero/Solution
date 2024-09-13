999385a3011a5e216f955675bf119ab352882559
Covenant <142973869+etrezero@users.noreply.github.com>
2024-09-13 15:45:29 +0900

    Update Covenant Model - AI Signal Trading.py

--- Covenant Model - AI Signal Trading.py
+++ Covenant Model - AI Signal Trading.py
@@ -1,28 +1,41 @@
+import FinanceDataReader as fdr
+import yfinance as yf
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 from dash.dash_table.Format import Format, Scheme
+import dash_table
 import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
-import FinanceDataReader as fdr
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from pykrx import stock as pykrx
 from scipy.optimize import minimize
-import dash_table
+import requests
+from sklearn.ensemble import IsolationForest
+
+
 
 # Dash 애플리케이션 생성
 app = dash.Dash(__name__)
-
 
 
 # 종목 코드 설정
 stock_code = {
     "252670": "KODEX 200선물인버스2X", 
     "122630": "KODEX 레버리지", 
+    "SOXX": "SOXX",
+    "SOXL": "SOXL", 
+    "SOXS": "SOXS", 
+    "UVXY": "UVXY", 
+    "SVXY": "SVXY", 
+    "TLT": "TLT",
+    "TMV": "TMV",
+
+
     "069500": "KODEX 200",
     "114800": "KODEX 인버스",
     "005930": "삼성전자",
@@ -30,39 +43,43 @@ stock_code = {
     "005380": "현대차",
     "207940": "삼성바이오로직스",
     "035420": "NAVER",
-    "068270": "셀트리온",
+    "101360": "에코앤드림",
+    "044450": "KSS해운",
     
-    "SOXX": "SOXX",
     "VUG": "VUG",
-    "VTV": "VTV",
-    "VEA": "VEA",
-    "VWO": "VWO",
     "SPY": "SPY",
-    "QQQ": "QQQ",
     "MAGS": "MAGS",
-    "DIA": "DIA",
-    "AAPL": "AAPL",
     "TSLA": "TSLA",
-    "NVDA": "NVDA",
-    "GOOG": "GOOG",
-    
-    "BND": "Total Bond",
-    "HYG": "High Yield Corporate Bond",
-    "TMV": "20+Treasury Bear 3X",
-    "TLT": "20+Treasury",
-    
+    "META": "META",
+
 }
 
 # 연도 선택을 위한 Dropdown 옵션 생성
 current_year = datetime.today().year
 year_options = [{'label': str(year), 'value': year} for year in range(current_year - 10, current_year + 1)]
 
+# SMA 윈도우별 변동성 계산 함수
+def calculate_volatility(df_price, stock_code_str):
+    if df_price is None or df_price.empty:
+        return None
+    volatilities = {}
+    df_price['SMA'] = df_price[stock_code_str].rolling(window=10).mean()
+    df_price['Strategy_Return'] = df_price[stock_code_str].pct_change().fillna(0)
+    volatilities = df_price['Strategy_Return'].std()
+    return min(volatilities, key=volatilities.get) if volatilities else None
+
+#
 
 # Layout 정의 (Dropdown 및 Loading 컴포넌트 추가)
 app.layout = html.Div(
-    style={'margin': 'auto', 'width': '75%', 'height': '100vh', 'display': 'flex', 'flexDirection': 'column'},
+    style={'margin': 'auto', 'width': '65%', 'height': '100vh', 'display': 'flex', 'flexDirection': 'column'},
     children=[
-        html.H1(f"Covenant AI Signal {datetime.today().strftime('%Y-%m-%d')}", style={'textAlign': 'center'}),        html.Div(id='buy-sell-signal', style={'fontSize': 24, 'textAlign': 'center', 'color': 'green'}),
+        html.H1(f"Covenant AI Signal {datetime.today().strftime('%Y-%m-%d')}", style={'textAlign': 'center'}),        
+       
+        html.Div(
+            id='buy-sell-signal', 
+            style={'fontSize': 24, 'textAlign': 'center', 'color': 'green'}),
+       
         html.Label("Select Stock:"),
         dcc.Dropdown(
             id='stock-dropdown',
@@ -77,16 +94,22 @@ app.layout = html.Div(
             value=current_year,
             style={'width': '40%'},
         ),
+    
         
-        
+
         dcc.Loading(
             id="loading-graph",
             type="default",
             children=html.Div([
                 dcc.Graph(id='cumulative-return-graph', style={'flex': '1'}),
-                dcc.Graph(id='signal-graph', style={'flex': '1'})
+                dcc.Graph(id='signal-graph', style={'flex': '1'}),
+                dcc.Graph(id='anomalies-graph', style={'flex': '1'}), 
             ], style={'display': 'flex', 'flexDirection': 'row'})
         ),
+
+
+
+
         dcc.Loading(
             id="loading-text",
             type="default",
@@ -95,8 +118,8 @@ app.layout = html.Div(
                 style={'height': '100px'}
             )
         ),
-        
-        
+
+
         dash_table.DataTable(
             id='data-table',
             style_cell={
@@ -109,13 +132,14 @@ app.layout = html.Div(
                 'width': '80px',     # 모든 셀의 고정 너비 설정
             },
             style_table={'width': '100%', 'overflowX': 'auto'},  # 테이블 크기와 스크롤 설정
-            page_size=30  # 페이지당 표시할 행 수를 30으로 설정
+            page_size=15  # 페이지당 표시할 행 수를 30으로 설정
         ),
 
 
-        
+
     ]
 )
+
 
 # 데이터 가져오기 함수
 def fetch_data(code, start, end):
@@ -125,13 +149,21 @@ def fetch_data(code, start, end):
                 code = '0' + code
             df_price = pykrx.get_market_ohlcv_by_date(start, end, code)['종가']
         else:
-            df_price = fdr.DataReader(code, start, end)['Adj Close']
+            session = requests.Session()
+            session.verify = False  # SSL 인증서 검증 비활성화
+            yf_data = yf.Ticker(code, session=session)
+            df_price = yf_data.history(start=start, end=end)['Close']
+
         df_price = pd.DataFrame(df_price)
         df_price.columns = [code]
+        df_price.index = pd.to_datetime(df_price.index).strftime('%Y-%m-%d')  # 인덱스를 %Y%m%d 형식으로 변환 후 문자열로 저장
+    
         return df_price
+    
     except Exception as e:
         print(f"Error fetching data for {code}: {e}")
         return None
+
 
 # LSTM 데이터를 생성하는 함수
 def create_lstm_data(df_price, target_col, time_steps=50):
@@ -156,6 +188,17 @@ def create_lstm_model(input_shape):
     model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
+
+# Isolation Forest로 이상치 감지 함수 수정
+def detect_anomalies_isolation_forest(df, contamination=0.5):
+    target_col = 'Close'
+    model = IsolationForest(contamination=contamination, random_state=42)
+    df['anomaly'] = model.fit_predict(df[[target_col]])
+    anomalies = df[df['anomaly'] == -1]
+    return anomalies
+
+
+
 # 최적화 함수 정의
 def optimize_weights(df_price):
     def calc_strategy(weights):
@@ -176,15 +219,29 @@ def optimize_weights(df_price):
 
 
 
+# Isolation Forest로 이상치 감지 함수=============================
+def detect_anomalies_isolation_forest(df, column_name, contamination=0.01):
+    model = IsolationForest(contamination=contamination, random_state=42)
+    df['anomaly'] = model.fit_predict(df[[column_name]])
+    anomalies = df[df['anomaly'] == -1]
+    return anomalies
+
+
+
+
+
 
 @app.callback(
     [Output('cumulative-return-graph', 'figure'),
      Output('signal-graph', 'figure'),
+     Output('anomalies-graph', 'figure'),  # 이상치 그래프 추가
      Output('optimal-weights-output', 'children'),
      Output('buy-sell-signal', 'children'),
-     
+
      Output('data-table', 'data'),  # 테이블 출력을 위한 Output 추가
-     Output('data-table', 'columns')],  # 테이블 출력
+     Output('data-table', 'columns'),  # 테이블 출력
+    ],
+
     [Input('stock-dropdown', 'value'),
      Input('year-dropdown', 'value')]
 )
@@ -201,11 +258,11 @@ def update_graph(stock_code, selected_year):
     df_price['Daily_Return'] = df_price[stock_code].pct_change().fillna(0)
 
     # Bollinger Bands 시그널
-    df_price['MA20'] = df_price[stock_code].rolling(window=20).mean()
-    df_price['Upper'] = df_price['MA20'] + (df_price[stock_code].rolling(window=20).std() * 2)
-    df_price['Lower'] = df_price['MA20'] - (df_price[stock_code].rolling(window=20).std() * 2)
+    df_price['MA20'] = df_price[stock_code].rolling(window=5).mean()
+    df_price['Upper'] = df_price['MA20'] + (df_price[stock_code].rolling(window=5).std() * 1)
+    df_price['Lower'] = df_price['MA20'] - (df_price[stock_code].rolling(window=5).std() * 1)
     df_price['BB_Signal'] = 0
-    df_price.loc[(df_price[stock_code] > df_price['Upper']), 'BB_Signal'] = 1
+    df_price.loc[(df_price[stock_code] > df_price['MA20']), 'BB_Signal'] = 1
     df_price.loc[(df_price[stock_code] < df_price['Lower']), 'BB_Signal'] = -1
 
     # SMA 시그널
@@ -222,11 +279,9 @@ def update_graph(stock_code, selected_year):
     predicted = lstm_model.predict(X)
     predicted = scaler.inverse_transform(predicted)
     padding_length = len(df_price) - len(predicted)
-
-    # LSTM 예측 결과를 1 또는 -1로 변환
     lstm_signals_resized = np.concatenate([np.zeros(padding_length), np.sign(predicted.flatten())])
-    df_price['LSTM_Signal'] = lstm_signals_resized
 
+    df_price['LSTM_Signal'] = lstm_signals_resized
 
     # 최적화된 가중치로 최종 시그널 생성 (시그널 값은 1 또는 0)
     optimal_weights = optimize_weights(df_price)
@@ -237,7 +292,8 @@ def update_graph(stock_code, selected_year):
     # 시그널 값이 1 또는 0으로만 설정되도록 처리
     df_price['Weighted_Signal_Final'] = df_price['Weighted_Signal'].apply(lambda x: 1 if x > 0 else 0)
 
-    # 포트폴리오 수익률 계산: 전날 시그널이 1일 때만 다음 날 수익률 반영, 시그널이 0일 때는 수익률 0
+    # 포트폴리오 수익률 계산: 시그널이 1일 때만 일간 수익률 반영, 시그널이 0일 때는 수익률 0
+    # 포트폴리오 수익률이 선택종목의 수익률을 넘을 수 없도록 설정
     df_price['Portfolio_Return'] = np.where(df_price['Weighted_Signal_Final'].shift(1) == 1, df_price['Daily_Return'], 0)
 
     # 선택된 기간 내에서 전략 누적 수익률을 0으로 시작하도록 조정
@@ -251,46 +307,82 @@ def update_graph(stock_code, selected_year):
     last_signal = df_price['Weighted_Signal_Final'].iloc[-1]
     signal_text = "매수" if last_signal == 1 else "매도"
 
-    # 누적 수익률 그래프 (전략과 선택한 종목의 누적 수익률을 함께 표시)
+
+
+
+    # 전략과 선택한 종목의 누적 수익률=======
     fig_cumulative = go.Figure()
 
-    # 포트폴리오 누적 수익률
+    # 전략포트폴리오 누적 수익률
     fig_cumulative.add_trace(go.Scatter(x=df_price.index, y=df_price['Portfolio_Cum_Return'], mode='lines', name='Covenant Portfolio'))
 
-    # 선택한 종목의 누적 수익률
+    # 선택종목의 누적 수익률
     fig_cumulative.add_trace(go.Scatter(x=df_price.index, y=df_price['Stock_Cum_Return'], mode='lines', name=f'{stock_code}'))
 
+
+
     fig_cumulative.update_layout(
-        title=f'Cumulative Return for {stock_code} ({selected_year})',
+        title=f'Cumulative Return for {stock_code} ({selected_year}) with Anomalies',
         xaxis_title='Date',
         yaxis_title='Cumulative Return',
         yaxis={'tickformat': '.0%'},
-        xaxis=dict(range=[df_price.index.min(), df_price.index.max()])  # 시작과 끝 날짜로 x-axis 범위 제한
+        yaxis2=dict(title="Anomalies", overlaying='y', side='right', showgrid=False),
+        xaxis=dict(range=[df_price.index.min(), df_price.index.max()])
     )
 
 
-    # 시그널 그래프
+
+    # 시그널 그래프=======================================
     fig_signal = go.Figure()
+
     fig_signal.add_trace(
         go.Scatter(
             x=df_price.index, 
             y=df_price['Weighted_Signal_Final'], 
-            mode='lines', name='Weighted Signal Final'))
+            mode='lines', name='Weighted Signal Final')
+    )
+    
     fig_signal.update_layout(
-        title=f'Weighted Signal Final for {stock_code} ({selected_year})',
+        title=f'Signal for {stock_code} ({selected_year})',
         xaxis_title='Date',
         yaxis_title='Signal (0 or 1)',
         xaxis=dict(range=[df_price.index.min(), df_price.index.max()])  # 시작과 끝 날짜로 x-axis 범위 제한
     )
 
+
+
+ # 이상치 그래프=======================================
+    
+
+    # 이상치 감지======================================================
+    anomalies = detect_anomalies_isolation_forest(df_price, stock_code, contamination=0.1)
+
+    fig_anomalies = go.Figure()
+
+    # 종목 가격 그래프 (Closing Price)
+    fig_anomalies.add_trace(go.Scatter(x=df_price.index, y=df_price[stock_code], mode='lines', name='Closing Price', line=dict(color='blue')))
+
+    # 이상치 표시 (Anomalies)
+    fig_anomalies.add_trace(go.Scatter(x=anomalies.index, y=anomalies[stock_code], mode='markers', name='Anomalies', marker=dict(color='red', size=10)))
+
+    fig_anomalies.update_layout(
+        title=f'{stock_code} Price with Anomalies ({selected_year})',
+        xaxis_title='Date',
+        yaxis_title='Price',
+        yaxis={'tickformat': ',.0f'},  # y축 포맷 설정
+        xaxis=dict(range=[df_price.index.min(), df_price.index.max()])
+    )
+
+
+
     weights_text = f"Optimal Weights: Bollinger: {optimal_weights[0]:.2f}, LSTM: {optimal_weights[1]:.2f}, SMA: {optimal_weights[2]:.2f}"
 
 
 
-    # 필요한 열만 선택하여 테이블로 표시
+  # 테이블로 표시 : 필요한 열만 선택하여 
     # 인덱스를 'Date'라는 이름의 열로 변환하여 추가
     df_selected = df_price[['Daily_Return', 'BB_Signal', 'SMA_Signal', 'LSTM_Signal', 'Weighted_Signal_Final', 'Strategy_Return', 'Cum_Return']].reset_index()
-    df_selected['날짜'] = pd.to_datetime(df_selected['날짜']).dt.strftime('%Y%m%d')  # 인덱스를 %Y%m%d 형식으로 변환
+    df_selected['날짜'] = df_price.index
 
     # '매수/매도' 열 추가: Weighted_Signal_Final이 1이면 '매수', 0이면 '매도'
     df_selected['매수/매도'] = df_selected['Weighted_Signal_Final'].apply(lambda x: "매수" if x == 1 else "매도")
@@ -306,16 +398,20 @@ def update_graph(stock_code, selected_year):
         *[
             {"name": col, "id": col, "type": "numeric", "format": Format(precision=2, scheme=Scheme.percentage)} 
             if 'return' in col.lower() else {"name": col, "id": col, "type": "numeric"}  # return이 포함된 열에 퍼센트 포맷 적용
-            for col in df_selected.columns if col not in ['날짜', '매수/매도', 'Daily_Return', 'Strategy_Return', 'Weighted_Signal_Final']  # 이미 추가된 열 제외
+            for col in df_selected.columns if col not in ['날짜', '매수/매도', 'Daily_Return', 'Strategy_Return', 'Weighted_Signal_Final', 'Date']  # 이미 추가된 열 제외
         ]
     ]
 
     table_data = df_selected.to_dict('records')
 
+    print(df_price)
 
 
 
-    return fig_cumulative, fig_signal, weights_text, signal_text, table_data, table_columns
+
+    return fig_cumulative, fig_signal, fig_anomalies, weights_text, signal_text, table_data, table_columns
+
+
 
 # 애플리케이션 실행
 if __name__ == '__main__':
