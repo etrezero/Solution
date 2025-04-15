b1f34b47d777c3c7e94e0a556587a0f4124a21ff
etrezero <etrezero@gmail.com>
2025-04-15 12:53:45 +0900

    Delete all files in the Solution folder

--- split.js
+++
@@ -1,9 +0,0 @@
-// Split.js 기본 구동
-document.addEventListener("DOMContentLoaded", function () {
-  Split(['#left-pane', '#right-pane'], {
-    sizes: [50, 50],
-    minSize: 200,
-    gutterSize: 8,
-    cursor: 'ew-resize',
-  });
-});
